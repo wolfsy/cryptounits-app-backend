8e3b3ca0d4cb49df5ab2a764ba2ed2beb666882f
Joncz <jonczyk.piotrr@gmail.com>
2023-01-15 00:46:03 +0100

    Added drf_yasg / swagger feature

--- App/urls.py
+++ App/urls.py
@@ -1,8 +1,24 @@
 from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from App import views 
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     re_path(r'^wallets$', views.wallet_list),
-    re_path(r'^wallet/(?P<id>[0-9]+)$', views.wallet_detail)
+    re_path(r'^wallet/(?P<id>[0-9]+)$', views.wallet_detail),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
--- crypocurrency_backend/settings.py
+++ crypocurrency_backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'drf_yasg',
     'rest_framework',
     'corsheaders',
     'App.apps.AppConfig'
