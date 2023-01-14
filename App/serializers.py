4abf985cbafd96a2865032ec47c1a6f1c5b418dc
Joncz <jonczyk.piotrr@gmail.com>
2023-01-14 20:35:45 +0100

    Serializers update and views initialization

--- App/serializers.py
+++ App/serializers.py
@@ -1,25 +1,26 @@
 from rest_framework import serializers
 from App.models import Wallet, Users, Crypto, Transactions
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
         fields = ('WalletId', 'WalletVault', 'WalletBTC', 'WalletETH', 'WalletUSDT', 'WalletBUSD', 'WalletSOL', 'WalletGALA',
                   'WalletXRP', 'WalletADA', 'WalletLTC', 'WalletDOGE', 'WalletBNB', 'WalletZIL', 'WalletMATIC', 'WalletAVAX') 
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ('UserId', 'UserRole', 'UserName', 'UserSurname', 'UserEmail', 'UserProfileURL', 'UserFacebookURL', 
                   'UserInstagramURL', 'UserTwitterURL', 'UserPassword', 'UserStatus', 'UserWallet')
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class CryptoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Crypto
         fields = ('CryptoId', 'CryptoIcon', 'CryptoName', 'CryptoAbbreviation', 'CryptoCurrentPrice')
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class TransactionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transactions
         fields = ('TransactionId', 'TransactionActionType', 'TransactionCryptoType', 'TransactionAmount', 
                   'TransactionTime', 'TransactionsUser')
+                  
--- App/views.py
+++ App/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from App.models import Wallet, Users, Crypto, Transactions
+from App.serializers import WalletSerializer, UsersSerializer, CryptoSerializer, TransactionsSerializer
 
 # Create your views here.
